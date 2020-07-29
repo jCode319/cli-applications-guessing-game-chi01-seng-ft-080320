adbac32802d342d4f280811b0bf62522c37e989c
Jarrell Jackson <jdjack3@gmail.com>
2020-07-29 18:24:20 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,23 @@
-# Code your solution here!
+require 'pry'
+
+ruby bin/guessing_game_cli
+
+def guessing_game_cli
+  
+end
+number = rand(1...6)
+guess = ""
+
+
+guess = gets.chomp()
+"guess a number between 1 and 6"
+
+
+if guess != number
+  puts "Sorry! The computer guessed <number>."
+  
+#if match - You guessed the correct number!
+
+if = exit 
+  puts "Goodbye!"
+
