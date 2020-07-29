3f559323bea32344ed9d81b064ccad328db6109d
Jarrell Jackson <jdjack3@gmail.com>
2020-07-29 18:34:52 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 def run_guessing_game
   puts "Guess a number between 1 and 6"
-  number = rand(1..6)
+  number = rand(1..6).to_s
 
   guess = gets.chomp
 
@@ -15,7 +15,7 @@ if guess != number
   puts "Sorry! The computer guessed <number>."
 end
 
-if user_input = exit
+if user_input = "exit"
   puts "Goodbye!"
 end
 
